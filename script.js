ccd6420ead2d7d838d565649f0754c08ba874d28
Yarden <Aharon Family@DESKTOP-FQIN86U>
2025-05-11 16:37:03 +0300

    Prevent duplicate items

--- script.js
+++ script.js
@@ -30,6 +30,11 @@ function onAddItemSubmit (e) {
         itemToEdit.classList.remove('edit-mode');
         itemToEdit.remove();
         isEditMode=false;
+    } else {
+        if (checkIfItemExists(newItem)) {
+            alert('That item is already exists!');
+            return;
+        }
     }
  
     addItemToDOM(newItem);
@@ -99,6 +104,12 @@ function onClickItem(e) {
         } else {
             setItemToEdit(e.target);
         }
+}
+
+function checkIfItemExists (item) {
+    const itemsFromStorage=getItemsFromStorage();
+
+    return itemsFromStorage.includes(item);
 }
 
 function setItemToEdit(item) {
